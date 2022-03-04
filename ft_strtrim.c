48682f9351fb1185235e5ee2b03cb7828ff73020
ndionis <ndionis@k1r3p7.42mulhouse.fr>
2022-03-04 21:02:49 +0100

    strtrim fonctionnel

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,11 +6,25 @@
 /*   By: supersko <ndionis@student.42mulhouse.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 14:33:46 by supersko          #+#    #+#             */
-/*   Updated: 2022/03/02 18:14:22 by supersko         ###   ########.fr       */
+/*   Updated: 2022/03/04 20:59:12 by supersko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int		is_trimable(char c, char *trimset)
+{
+	int	ii;
+
+	ii = 0;
+	while (trimset[ii])
+	{
+		if (trimset[ii] == c)
+			return (1);
+		ii++;
+	}
+	return (0);
+}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -20,19 +34,31 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char			*ret;
 
 	start = 0;
-	end = ft_strlen( (char *) s1);
-	while (s1[start] && ft_strchr( (char *) set, s1[start]))
+	end = ft_strlen( (char *) s1) - 1;
+	while (s1[start] && is_trimable((char) s1[start], (char *) set))
 		start++;
-	while (end > start && ft_strchr( (char *) set, s1[end]))
+	while (end > start &&  is_trimable((char) s1[end], (char *) set))
 		end--;
 	ret = malloc(sizeof(char *) * (end - start + 1));
 	if (!ret)
 		return (NULL);
 	ii = 0;
-	while (start < end)
+	while (start <= end--)
 	{
 		ret[ii] = s1[start + ii];
 		ii++;
 	}
+	ret[ii] = '\0';
 	return (ret);
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char s[100];
+
+	strcpy(s, "-   asd asd   dfgh     --");
+	printf("[%s] -> [%s]\n", s, ft_strtrim(s, " -"));
 }
