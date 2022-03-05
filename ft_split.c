a647d2471e3823205772d3528d920c52841889ca
ndionis <ndionis@k1r3p7.42mulhouse.fr>
2022-03-05 11:43:31 +0100

    ft_wdcnt ok

--- ft_split.c
+++ ft_split.c
@@ -6,12 +6,24 @@
 /*   By: supersko <ndionis@student.42mulhouse.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 16:31:55 by supersko          #+#    #+#             */
-/*   Updated: 2022/03/02 18:35:54 by supersko         ###   ########.fr       */
+/*   Updated: 2022/03/05 11:42:41 by supersko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+static int      is_trimable(char c, char *trimset)
+{
+    int ii;
 
+    ii = 0;
+    while (trimset[ii])
+    {
+        if (trimset[ii] == c)
+            return (1);
+        ii++;
+    }
+    return (0);
+}
 static unsigned int	ft_strlench(char const *s, char c)
 {
 	unsigned int	ii;
@@ -29,32 +41,32 @@ static unsigned int	ft_strlench(char const *s, char c)
 // un poteau (mot) de plus que de fils :
 // |mot|sep|mot| => 1 sep = 2 mots
 
-static unsigned int	ft_wdcnt(char *s, int c)
+static unsigned int	ft_wdcnt(char *s, char *c)
 {
 	unsigned int	ii;
 	unsigned int	count;
-	unsigned int	end;
 
-	ii = 0;
 	count = 0;
-	end = ft_strlen(s);
-	while (!s[end] || s[end] == c)
-		end--;
-	while (s[ii] == c)
-		ii++;
-	while (ii <= end)
+	ii = 0;
+	while (s[ii])
 	{
-		if (s[ii] == c)
+		if (is_trimable(s[ii], c))
 		{
-			count++;
-			while (s[ii] == c)
+			ii++;
+			while (is_trimable(s[ii], c))
 				ii++;
 		}
-		ii++;
+		else
+		{
+			count++;
+			while (!is_trimable(s[ii], c) && s[ii])
+				ii++;
+		}
 	}
-	return ++count;
+	return count;
 }
 
+// Desole pour la conversion de char en string
 char **ft_split(char const *s, char c)
 {
 	char			**ret;
@@ -62,8 +74,11 @@ char **ft_split(char const *s, char c)
 	unsigned int	wd_ind;
 	unsigned int	letter_ind;
 	unsigned int	nb_wd;
+	char			c_cpy[2];
 
-	nb_wd = ft_wdcnt((char *) s, c);
+	c_cpy[0] = c;
+	c_cpy[1] = '\0';
+	nb_wd = ft_wdcnt((char *) s, c_cpy);
 	ret = (char **) malloc((++nb_wd) * sizeof(char*));
 	*(ret + nb_wd) = (char *) NULL;
 	wd_ind = 0;
@@ -85,12 +100,12 @@ char **ft_split(char const *s, char c)
 	}
 	return (ret);
 }
-/*
-//
 #include <stdio.h>
+#include <string.h>
 // MAIN
 int main(void)
 {
+	/*
 	char	**sortie;
 	int		ii;
 
@@ -98,5 +113,22 @@ int main(void)
 	ii = 0;
 	while (sortie[ii] != NULL)
 		 printf("%s\n", sortie[ii++]); 
-}
 */
+	char s[10];
+	strcpy(s, "");
+	printf("for [%s] -> %d\n", s, ft_wdcnt(s, " "));
+	strcpy(s, "  ");
+	printf("for [%s] -> %d\n", s, ft_wdcnt(s, " "));
+	strcpy(s, "a");
+	printf("for [%s] -> %d\n", s, ft_wdcnt(s, " "));
+	strcpy(s, "a ");
+	printf("for [%s] -> %d\n", s, ft_wdcnt(s, " "));
+	strcpy(s, " a");
+	printf("for [%s] -> %d\n", s, ft_wdcnt(s, " "));
+	strcpy(s, " a ");
+	printf("for [%s] -> %d\n", s, ft_wdcnt(s, " "));
+	strcpy(s, "  aa  ");
+	printf("for [%s] -> %d\n", s, ft_wdcnt(s, " "));
+	strcpy(s, "bb   bb");
+	printf("for [%s] -> %d\n", s, ft_wdcnt(s, " "));
+}
