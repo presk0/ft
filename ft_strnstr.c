7cd426b6dfbb33cb07c89c617b582397382e52e8
ndionis <ndionis@k1r3p7.42mulhouse.fr>
2022-03-10 14:57:40 +0100

    strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,23 +6,53 @@
 /*   By: supersko <ndionis@student.42mulhouse.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 15:51:11 by supersko          #+#    #+#             */
-/*   Updated: 2022/03/09 19:06:05 by supersko         ###   ########.fr       */
+/*   Updated: 2022/03/10 14:55:08 by supersko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+// from nickdotht 
+char	*ft_strnstr(const char *str, const char *ndl, size_t len)
+{
+	size_t		i_str;
+	size_t		i_ndl;
+	size_t		i_cmp;
+	int			found;
+
+	i_str = -1;
+	found = 1;
+	if (!ft_strlen(ndl))
+		return ((char *)str);
+	while (*(str + ++i_str) && i_str < len)
+	{
+		i_ndl = 0;
+		if (*(str + i_str) == *(ndl))
+		{
+			i_cmp = i_str;
+			found = 1;
+			while (*(str + i_cmp) && *(ndl + i_ndl) && i_ndl < len && i_cmp < len)
+				if (*(str + i_cmp++) != *(ndl + i_ndl++))
+					found = 0;
+			if (found && !*(ndl + i_ndl))
+				return ((char *)str + i_str);
+		}
+	}
+	return (NULL);
+}
+
+/*
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	unsigned int	len_needle;
 	unsigned int	ii;
 	unsigned int	jj;
 
-	if (!haystack)
-		return (NULL);
 	len_needle = ft_strlen(needle);
 	if (!len_needle)
 		return ((char *) haystack);
+	if (!haystack)
+		return (NULL);
 	if (len < len_needle)
 		return (NULL);
 	else
@@ -40,6 +70,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	}
 	return (NULL);
 }
+*/
 
 /*
 //
